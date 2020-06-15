0b3f21bc771d902ab7c39b9d8bc737ff50ba96c3
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-06-15 17:53:00 +0300

    short story

--- story.js
+++ story.js
@@ -1,7 +1,12 @@
 // Define the name here on line 2
-
+let name = "Bader";
 // Define the age here on line 4
-
+let age = 38;
 // Define the hobby here on line 6
-
+let hobby = "writing Sci-fi novels";
 // Print the story here on line 8
+console.log(
+  `Mr./Ms. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old, by then you will decades of ${hobby} practice! `
+);
